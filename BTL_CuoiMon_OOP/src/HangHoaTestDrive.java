9e6b7ff79c3690a3824a84a98fd2ec2c64aea8c5
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2022-04-19 16:00:44 +0700

    Day 06

--- BTL_CuoiMon_OOP/src/HangHoaTestDrive.java
+++ BTL_CuoiMon_OOP/src/HangHoaTestDrive.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 public class HangHoaTestDrive {
     public static void main(String[] args) throws IOException {
         // FileWriter.write("Data Chuong Trinh", "Hello");
-        // ControllerHangHoa controllerHangHoa = new ControllerHangHoa();
-        // DanhSachHangHoa danhSachHangHoa = new DanhSachHangHoa();
-        // String luaChon = null;
-        // do{
-        //     HangHoa hangHoa = controllerHangHoa.inputHangHoa();
-        //     danhSachHangHoa.themHangHoa(hangHoa);
-        //     System.out.println("Nhập Tiếp Không [Y/N]");
-        //     controllerHangHoa.sc.nextLine();
-        //     luaChon = controllerHangHoa.sc.nextLine();
-        // }while(luaChon.equals("Y") || luaChon.equals("y"));
-        // danhSachHangHoa.hienThiHangHoa();
+        ControllerHangHoa controllerHangHoa = new ControllerHangHoa();
+        DanhSachHangHoa danhSachHangHoa = new DanhSachHangHoa();
+        String luaChon = null;
+        do{
+            HangHoa hangHoa = controllerHangHoa.inputHangHoa();
+            danhSachHangHoa.themHangHoa(hangHoa);
+            System.out.println("Nhập Tiếp Không [Y/N]");
+            controllerHangHoa.sc.nextLine();
+            luaChon = controllerHangHoa.sc.nextLine();
+        }while(luaChon.equals("Y") || luaChon.equals("y"));
+        danhSachHangHoa.hienThiHangHoa();
 
         // String c = "C:/Windows/System32/calc.exe";
         // Runtime env = Runtime.getRuntime();
